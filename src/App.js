52090f4b28e46f30ae2c1c5a1d72487b98f24fa6
jecrs687 <emanuelcascone@gmail.com>
2020-02-13 16:37:32 +0100

    design inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+function Button({children, onClick}){
+  const [isSelected, select]= useState(false) 
+  return(
+      <button 
+      style={{backgroundColor:isSelected? '#bbb': '#eee'}} 
+      onClick={()=>{onClick(children);select(!isSelected)}} >
+        {children}
+      </button>
+    )
+}
 function App() {
+  const [value, setValue] = useState("")
+  const [selecteds, setSelects]= useState([]) 
+  var text;
+  function handleclick(value){
+    value = value.props? value.props.children:value;
+    if(selecteds.indexOf(value)===-1){
+      setSelects([...selecteds,value])
+    }else{
+      var temp = selecteds;
+      temp.splice(selecteds.indexOf(value),1);
+      setSelects(temp)
+    }
+    console.log(selecteds)
+  }
+  function write({target}){
+    text = target.value
+    console.log(text)
+    setValue(target.value)
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-head">
+        <Button onClick={handleclick}><strong>n</strong></Button>
+        <Button onClick={handleclick}><i>i</i></Button>
+        <Button onClick={handleclick}>size+</Button>
+        <Button onClick={handleclick}>size-</Button>
+      </div>
+      <div className="App-body"> 
+        <textarea className="App-box"  type="text" value={value} onChange={write} />
+        <div className='App-box'>
+          <texto text={text}/>
+        </div>
+      </div>
+      <div className="App-footer">
+        this is an open-source project by: 
+        <a href="https://jecrs687.github.io/"> @jecrs687</a>
+      </div>
     </div>
   );
 }
