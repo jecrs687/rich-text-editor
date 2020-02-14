76e9e625c9276c2fcc9beea0177b21aac0925b5d
jecrs687 <emanuelcascone@gmail.com>
2020-02-14 11:51:16 +0100

    the first project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { useState } from 'react';
+
 function Button({children, onClick}){
   const [isSelected, select]= useState(false) 
   return(
@@ -11,39 +12,103 @@ function Button({children, onClick}){
       </button>
     )
 }
+function Button2({children, onClick}){
+  return(
+      <button  
+      style={{backgroundColor: '#eee'}} 
+
+      onClick={()=>{onClick(children);}} >
+        {children}
+      </button>
+    )
+}
+
+function Text({text}){
+
+  return(
+    <p>{text}</p>
+  )
+}
+
 function App() {
   const [value, setValue] = useState("")
-  const [selecteds, setSelects]= useState([]) 
+  // const [selecteds, setSelects]= useState([]) 
+  const [reload, setReload] = useState(true)
+  const [style, setStyle] = useState({
+    fontSize:14,
+    fontWeight:'300',
+    fontStyle:'normal'
+  })
   var text;
   function handleclick(value){
     value = value.props? value.props.children:value;
-    if(selecteds.indexOf(value)===-1){
-      setSelects([...selecteds,value])
-    }else{
-      var temp = selecteds;
-      temp.splice(selecteds.indexOf(value),1);
-      setSelects(temp)
-    }
-    console.log(selecteds)
+    var temp=style;
+    console.log({...style})
+    console.log(value)
+    switch(value){
+      case 'i':
+        if(temp.fontStyle==='normal'){
+          temp.fontStyle='italic'
+          setStyle(temp)
+        }else{
+         temp.fontStyle='normal'
+          setStyle(temp)
+        }
+        break;
+      case "size+":
+        temp.fontSize=temp.fontSize+1
+        setStyle(temp)
+        break;
+      case "size-":
+        temp.fontSize=temp.fontSize-1
+        setStyle(temp)
+        break;
+      case "n":
+        if(temp.fontWeight==='300'){
+         temp.fontWeight='900'
+          setStyle(temp)}
+        else{temp.fontWeight='300'
+          setStyle(temp)}
+        break;
+      default:
+        break
+      }
+      setReload(!reload)
+    // if(selecteds.indexOf(value)===-1){
+    //   setSelects([...selecteds,value])
+    // }else{
+    //   temp = selecteds;
+    //   temp.splice(selecteds.indexOf(value),1);
+    //   setSelects(temp)
+    // }
+    // console.log(selecteds)
   }
   function write({target}){
     text = target.value
-    console.log(text)
+    console.log(value)
+
     setValue(target.value)
 
   }
   return (
     <div className="App">
       <div className="App-head">
-        <Button onClick={handleclick}><strong>n</strong></Button>
-        <Button onClick={handleclick}><i>i</i></Button>
-        <Button onClick={handleclick}>size+</Button>
-        <Button onClick={handleclick}>size-</Button>
+        <div className="App-head-box">
+
+          <Button onClick={handleclick}><strong>n</strong></Button>
+          <Button onClick={handleclick}><i>i</i></Button>
+          <Button2 onClick={handleclick}>size+</Button2>
+          <Button2 onClick={handleclick}>size-</Button2>
+        </div>
+        <div className="App-head-box">
+          <h4>fontSize: {style.fontSize} | fontStyle: {style.fontStyle} | fontWeight: {style.fontWeight}</h4>
+
+         </div>
       </div>
       <div className="App-body"> 
-        <textarea className="App-box"  type="text" value={value} onChange={write} />
+        <textarea style={{...style}} className="App-box"  type="text" value={value} onChange={write} />
         <div className='App-box'>
-          <texto text={text}/>
+          <Text text={text}/>
         </div>
       </div>
       <div className="App-footer">
