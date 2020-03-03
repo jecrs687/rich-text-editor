f8db166a68925f2e7f592f1a8340c4a251cd416b
jecrs687 <emanuelcascone@gmail.com>
2020-03-03 19:28:55 -0300

    beta MUITO MELHOR

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect, useRef} from 'react';
 import './App.css';
 
 function App() {
-  const [text, setText] = useState();
-  function Words({value}){
-   
-    return(
-      <div 
-      contentEditable
-      // onInput={(value)=>{console.log(value)}}
-      onWheel={(value)=>{console.log(value.type)}}
-      >
-        {value}    
-      </div>
-      )
-  }
+  const [text, setText]        = useState('');
+  const boxText                = useRef()
+  const [textDiv, setTextDiv]  = useState('');
+
+  const [selected, setSelected] = useState({
+      "start": 0,
+      "end"  : 0
+  })
+
+  document.onselectionchange = () => {
+
+   var start = document.getSelection().anchorOffset - document.getSelection().extentOffset>0?document.getSelection().extentOffset:document.getSelection().anchorOffset
+   var end   = document.getSelection().extentOffset - document.getSelection().anchorOffset>0?document.getSelection().extentOffset:document.getSelection().anchorOffset
+
+    setSelected({
+      "start":start,
+      "end"  :end,
+
+    })  };
+
+    useEffect(()=>{
+      text.split('  *.&**  ');
+    },[text])
+
+// main code
   return (
     <div className="App">
-       <Words value={text}/>
+      <div>
+        <button onClick={
+          ()=>{
+            
+            var temp = textDiv.slice(0, selected.start)+ '  *.&**  ' + textDiv.slice(selected.start, selected.end) + '  *.&**  ' +  textDiv.slice(selected.end, textDiv.length);
+            setText(temp)
+          }
+        }><strong>N</strong></button>
+                <button onClick={
+          ()=>{
+            var temp = textDiv.slice(0, selected.start)+ '  *.&||  ' + textDiv.slice(selected.start, selected.end) + '  *.&||  ' +  textDiv.slice(selected.end, textDiv.length);
+            setText(temp)
+          }
+        }><i>I</i></button>
+      </div>
+      <div
+      className='box-text'
+      contentEditable
+      ref={boxText}
+      suppressContentEditableWarning={true}
+      children={text}
+      onInput={(value)=>{setTextDiv(value.currentTarget.textContent)}}
+        />       
+      <button onClick={()=>{console.log()}}>teste ....</button>
     </div>
   );
+// end main code
+
+
 }
 
 export default App;
