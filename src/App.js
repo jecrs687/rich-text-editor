6048434d5842091305452d0dfbfbf1a9eefddbe3
jecrs687 <emanuelcascone@gmail.com>
2020-05-23 22:38:28 -0300

    design update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React,{useState, useEffect, useRef} from 'react';
 import './App.css';
 
 function App() {
-  const [text, setText]          = useState('');
-  const [text2, setText2]        = useState('');
+  const [text, setText]          = useState({});
   const boxText                  = useRef()
-  const [textDiv, setTextDiv]    = useState('');
 
   const [selected, setSelected]  = useState({
       "start": 0,
@@ -14,48 +12,36 @@ function App() {
 
   document.onselectionchange = () => {
 
-   var start = document.getSelection().anchorOffset - document.getSelection().extentOffset>0?document.getSelection().extentOffset:document.getSelection().anchorOffset
-   var end   = document.getSelection().extentOffset - document.getSelection().anchorOffset>0?document.getSelection().extentOffset:document.getSelection().anchorOffset
-
+   var start = window.getSelection().anchorOffset - document.getSelection().extentOffset>0?document.getSelection().extentOffset:document.getSelection().anchorOffset
+   var end   = window.getSelection().extentOffset - document.getSelection().anchorOffset>0?document.getSelection().extentOffset:document.getSelection().anchorOffset
     setSelected({
       "start":start,
       "end"  :end,
 
     })  };
 
-    useEffect(()=>{
-      boxText.current.innerHTML = text
-      console.log(boxText.current.innerHTML)
-    },[text])
+    useEffect(()=>{  
+      // boxText.getSelection()
+    },[])
 
 // main code
   return (
     <div className="App">
-      <div>
-        <button onClick={
-          ()=>{
-            
-            var temp = textDiv.slice(0, selected.start)+ '<strong>' + textDiv.slice(selected.start, selected.end) + '</strong>' +  textDiv.slice(selected.end, textDiv.length);
-
-            setText(temp)
-          }
-        }><strong>N</strong></button>
-                <button onClick={
-          ()=>{
-            var temp = textDiv.slice(0, selected.start)+ '<i>' + textDiv.slice(selected.start, selected.end) + '</i>' +  textDiv.slice(selected.end, textDiv.length);
-            setText(temp)
-          }
-        }><i>I</i></button>
+      <div className="top">
+        <button><strong>N</strong></button>
+        <button><i>I</i></button>
+        <input type="text" placeholder="size" defaultValue="12"></input>
       </div>
       <div
-      className='box-text'
-      contentEditable
-      ref={boxText}
-      children={text}
-      suppressContentEditableWarning
-      onInput={(value)=>{setTextDiv(value.currentTarget.textContent)}}
-        />       
-      <button onClick={()=>{console.log()}}>teste ....</button>
+          className='box-text'
+          placeholder="insert text"
+          contentEditable
+          ref={boxText}
+          onSelectCapture={(value)=>{console.log(selected)}}
+          // onInput={()=>{console.log(boxText.current.childNodes)}}
+          suppressContentEditableWarning
+      />       
+      <button>teste ....</button>
     </div>
   );
 // end main code
